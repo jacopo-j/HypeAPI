a46d19486f1dd8e2b1a54a751049c2bfd6fbc388
Jacopo Jannone <jacopo-j@users.noreply.github.com>
2020-02-29 02:05:52 +0100

    HYPE login improvements & token renewal

--- hype.py
+++ hype.py
@@ -6,6 +6,7 @@ from uuid import uuid4
 import json
 import requests
 from utils import loginrequired
+from datetime import datetime, date
 
 
 class Hype(Banking):
@@ -26,14 +27,25 @@ class Hype(Banking):
     def __init__(self):
         self._username = None
         self.newids = None
+        self.bin = None
+        self.checksum = None
         super().__init__()
 
-    def login(self, username, password):
+    def login(self, username, password, birthdate):
+        if isinstance(birthdate, datetime) or isinstance(birthdate, date):
+            dob = birthdate.strftime("%d/%m/%Y")
+        elif isinstance(birthdate, str):
+            dob = datetime.fromisoformat(birthdate).strftime("%d/%m/%Y")
+        elif birthdate is None:
+            dob = None
+        else:
+            raise ValueError("Invalid birth date")
         enroll1 = self._session.post(
             self.ENROLL_URL,
             data={
                 "additionalinfo": self.DEVICE_INFO,
                 "codiceinternet": username,
+                "datanascita": dob,
                 "deviceid": self.DEVICE_ID,
                 "function": "FREE/LOGINFIRSTSTEP.SPR",
                 "pin": password,
@@ -65,6 +77,7 @@ class Hype(Banking):
             raise self.RequestException("Failed to parse response for bioToken request")
         except KeyError:
             raise self.AuthenticationError("Missing data in response for bioToken request")
+        self.bin = enroll2.json()["Bin"]
         self._username = username
 
     def otp2fa(self, code):
@@ -85,18 +98,68 @@ class Hype(Banking):
         try:
             if otp.json()["Check"] != "OK":
                 raise self.AuthenticationError("OTP verification failed. Please login() again")
-            self.token = self._session.cookies.get_dict()["token"]
-            self.newids = self._session.cookies.get_dict()["newids"]
-            self._session = requests.Session()
-            self._session.headers.update({
-                "hype_token": self.token,
-                "newids": self.newids,
-                "App-Version": self.APP_VERSION
-            })
         except json.decoder.JSONDecodeError:
             raise self.RequestException("Failed to parse response for OTP verification request")
         except KeyError:
             raise self.AuthenticationError("OTP verification failed. Please login() again")
+        self.checksum = otp.json()["Checksum"]
+        self.token = self._session.cookies.get_dict()["token"]
+        self.newids = self._session.cookies.get_dict()["newids"]
+        self._session = requests.Session()
+        self._session.headers.update({
+            "hype_token": self.token,
+            "newids": self.newids,
+            "App-Version": self.APP_VERSION
+        })
+
+    @loginrequired
+    def renew(self):
+        """
+        Token renewal
+        """
+        renewal = self._session.post(
+            self.ENROLL_URL,
+            data={
+                "additionalinfo": self.DEVICE_INFO,
+                "bin": self.bin,
+                "checksum": self.checksum,
+                "deviceid": self.DEVICE_ID,
+                "function": "FREE/LOGINFIRSTSTEPFA.SPR",
+                "platform": "IPHONE"
+            },
+            timeout=5
+        )
+        try:
+            if renewal.json()["Check"] != "OK":
+                raise self.AuthenticationError("Renewal failed")
+        except json.decoder.JSONDecodeError:
+            raise self.AuthenticationError("Failed to parse response for renewal request")
+        except KeyError:
+            raise self.AuthenticationError("Renewal failed")
+        reenroll = self._session.post(
+            self.ENROLL_URL,
+            data={
+                "additionalinfo": self.DEVICE_INFO,
+                "deviceid": self.DEVICE_ID,
+                "function": "INFO/ENROLLBIO.SPR",
+                "platform": "IPHONE"
+            },
+            timeout=5
+        )
+        try:
+            if reenroll.json()["ErrorMessage"] != "":
+                raise self.AuthenticationError("Server returned error: " + reenroll.json()["ErrorMessage"])
+        except json.decoder.JSONDecodeError:
+            raise self.RequestException("Failed to parse response for bioToken request")
+        except KeyError:
+            raise self.AuthenticationError("Missing data in response for bioToken request")
+        self.token = self._session.cookies.get_dict()["token"]
+        self.newids = self._session.cookies.get_dict()["newids"]
+        self._session.headers.update({
+            "hype_token": self.token,
+            "newids": self.newids
+        })
+        self.bin = reenroll.json()["Bin"]
 
     @loginrequired
     def get_movements(self, limit=5):
